c77d80cf73583c85b103c3407532ac389ae79b96
ToJestKrzysio <krzysztof.plonka64@gmail.com>
2021-12-31 18:23:44 +0100

    PersonBuilder class

--- builder/02_person_builder.py
+++ builder/02_person_builder.py
@@ -1,7 +1,9 @@
+from __future__ import annotations
+
+
 class Person:
     name: str
     surname: str
-    id: str
     street_address: str
     postal_code: str
     city: str
@@ -9,12 +11,36 @@ class Person:
     def __init__(self):
         self.name = ""
         self.surname = ""
-        self.id = ""
 
         self.street_address = ""
         self.postal_code = ""
         self.city = ""
 
     def __str__(self) -> str:
-        return (f"{self.name} {self.surname} using personal id {self.id}\n"
+        return (f"{self.name} {self.surname}\n"
                 f"Living at {self.street_address}, {self.postal_code}, {self.city}")
+
+
+class PersonBuilder:
+
+    def __init__(self, person: Person | None = None):
+        self.person = person if person else Person()
+
+    @property
+    def named(self) -> PersonNameBuilder:
+        return PersonNameBuilder(self.person)
+
+    @property
+    def lives(self) -> PersonAddressBuilder:
+        return PersonAddressBuilder(self.person)
+
+    def build(self) -> Person:
+        return self.person
+
+
+class PersonNameBuilder(PersonBuilder):
+    pass
+
+
+class PersonAddressBuilder(PersonBuilder):
+    pass
