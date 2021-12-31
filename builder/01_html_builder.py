f36c40ccadf670d41d3791301c23d0831c007f6d
ToJestKrzysio <krzysztof.plonka64@gmail.com>
2021-12-31 18:02:14 +0100

    Fixes for HtmlBuilder class

--- builder/01_html_builder.py
+++ builder/01_html_builder.py
@@ -7,19 +7,24 @@ class HtmlTag:
 
     INDENT = 2
 
-    def __init__(self, tag: str):
+    def __init__(self, tag: str, value: str = ""):
         self.tag = tag
+        self.value = value
         self.elements = []
 
     def _show(self, indent: int) -> str:
         lines = []
-        line_indent = type(self).INDENT * indent * " "
-        lines.append(f"{line_indent}<{self.tag}>")
+        tag_indent = type(self).INDENT * indent * " "
+        lines.append(f"{tag_indent}<{self.tag}>")
+
+        if self.value:
+            value_indent = type(self).INDENT * (indent+1) * " "
+            lines.append(f"{value_indent}{self.value}")
 
         for element in self.elements:
             lines.append(element._show(indent + 1))
 
-        lines.append(f"{line_indent}</{self.tag}>")
+        lines.append(f"{tag_indent}</{self.tag}>")
         return "\n".join(lines)
 
     def __str__(self) -> str:
@@ -27,19 +32,31 @@ class HtmlTag:
 
 
 class HtmlBuilder:
+    _parent_tag: str
+    _parent_value: str
+    _parent: HtmlTag
 
-    def __init__(self, tag: str):
+    def __init__(self, tag: str, value: str = ""):
         self._parent_tag = tag
-        self._parent = HtmlTag(tag)
+        self._parent_value = value
+        self._parent = HtmlTag(tag, value)
 
-    def add_child(self, child_tag: str) -> HtmlBuilder:
-        self._parent.elements.append(
-            HtmlTag(child_tag)
-        )
+    def add_child(self, child_tag: str | HtmlBuilder, child_value="") -> HtmlBuilder:
+        appended_child = (child_tag._parent if isinstance(child_tag, HtmlBuilder)
+                          else HtmlTag(child_tag, child_value))
+        self._parent.elements.append(appended_child)
         return self
 
     def clear(self):
-        self._parent = HtmlTag(self._parent_tag)
+        self._parent = HtmlTag(self._parent_tag, self._parent_value)
 
     def __str__(self) -> str:
         return str(self._parent)
+
+
+if __name__ == '__main__':
+    builder = HtmlBuilder("ul")
+    builder.add_child("li", "Hello")
+    builder.add_child("li", "World")
+    builder.add_child(HtmlBuilder("p", "Hi"))
+    print(builder)
